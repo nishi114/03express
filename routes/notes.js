2185e000dc86d78e4f284ba6bb48377a5120142c
nishi114 <nishikawa.a.student@gmail.com>
2026-06-02 19:17:30 +0900

    Add mongodb

---
+++ routes/notes.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+// 接続情報を設定
+const {MongoClient} =require("mongodb");
+const uri = "kemekeme";
+const client = new MongoClient(uri);
+
+router.get('/',async(req,res) => {
+// データベース、コレクションを指定
+const database = client.db('notes');
+const notes = database.collection('notes');
+
+// idが1のドキュメントを取得
+const query = {id:1};
+const note = await notes.findOne(query);
+
+res.json(note.title);
+})
+
+module.exports = router;

230dc57266cb9bca9dc9edcf86ed3661b245e4d3
nishi114 <nishikawa.a.student@gmail.com>
2026-06-02 20:21:51 +0900

    Add cat api

---
+++ routes/cat.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+const request =require("request");
+
+
+router.get('/',async(req,res) => {
+request('https://api.thecatapi.com/v1/images/search', function (error, respose, body) {
+    if (!error && respose.statusCode == 200) {
+        const data = JSON.parse(body);
+        res.json(data);
+    }
+});
+})
+module.exports = router;

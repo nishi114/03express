6ae450c5ae4b0e50c75307003fc68506b2c57c6c
nishi114 <nishikawa.a.student@gmail.com>
2026-07-18 23:47:34 +0900

    add env

--- routes/notes_from_b.js
+++ routes/notes_from_b.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var router = express.Router();
 const cors = require('cors'); // corsミドルウェアを追加
+require('dotenv').config();
 
 // 接続情報を設定
 const {MongoClient} =require("mongodb");
 const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri);
+
+router.use(cors());
 
 router.get('/',async(req,res) => {
 // データベース、コレクションを指定
@@ -13,10 +16,9 @@ const database = client.db('notes');
 const notes = database.collection('notes');
 
 // idが1のドキュメントを取得
-const query = {id:1};
-const note = await notes.findOne(query);
+const note = await notes.find({}).toArray();
 
-res.json(note.title);
+res.json(note);
 })
 
 module.exports = router;

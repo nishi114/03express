25821df8caa66d3d826ce9f737f222b9ae85f685
nishi114 <nishikawa.a.student@gmail.com>
2026-07-19 23:00:25 +0900

    check3

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/hello', helloRouter);
-// app.use('/notes', notesRouter);
+app.use('/notes', notesRouter);
 app.use('/cat', catRouter);
 app.use('/dog', dogRouter);
 app.use('/notes_from_b', notes_from_bRouter);
